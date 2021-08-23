3a0a4ea5fb86f6cdf4004efae05da0f8ad62a450
MinhajulAkib <00akibswe@gmail.com>
2021-08-23 22:21:34 +0600

    product increase

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,5 +1,5 @@
-function updateCaseNumber(isIncreasing){
-    let caseInput = document.getElementById('case-number');
+function updateCaseNumber(product, price, isIncreasing){
+    let caseInput = document.getElementById(product + '-number');
     let caseNumber = caseInput.value;
     if(isIncreasing){
         caseNumber = parseInt(caseNumber) + 1;
@@ -9,20 +9,31 @@ function updateCaseNumber(isIncreasing){
     }
     caseInput.value = caseNumber;
     //update case total
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber *59;
-    
+    const caseTotal = document.getElementById(product + '-total');
+    caseTotal.innerText = caseNumber * price;
 }
 
+//phone increase decrase events
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updateCaseNumber('phone',true);
+});
+
+//phone minus
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateCaseNumber('phone',false);
+})
+
+
+//handle case increase decrase event
 document.getElementById('case-plus').addEventListener('click', function(){
-    updateCaseNumber(true);
+    updateCaseNumber('case', 59, true);
 
     
 });
 
 //minus 
 document.getElementById('case-minus').addEventListener('click', function(){
-    updateCaseNumber(false);
+    updateCaseNumber('case', 59,false);
     
     
 });
